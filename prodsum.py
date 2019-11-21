1cc6c4db955caf0785af809db7bb384ca7818025
Artem <artger@ukr.net>
2019-11-22 00:56:03 +0200

    test passed

--- prodsum.py
+++ prodsum.py
@@ -1,6 +1,21 @@
+'''
+Module
+'''
 from typing import List
 
 
-def prodsum(l: List[int]) -> int:
+def prodsum(list_elem: List[int]) -> int:
+    '''
 
-    return 42
+    :param list_elem:
+    :return:
+    '''
+    if len(list_elem) == 0:
+        return 0
+    product_elem = 1
+    sum_elem = 0
+    for i in list_elem[::2]:
+        product_elem *= i
+    for i in list_elem[1::2]:
+        sum_elem += i
+    return product_elem - sum_elem
