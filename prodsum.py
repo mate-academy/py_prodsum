c5cfd461fc2ecd5b424ed5e084310fec9564edcb
alexander <earthhuman41@gmail.com>
2019-11-22 01:20:17 +0200

    implemented prodsum function

--- prodsum.py
+++ prodsum.py
@@ -1,6 +1,25 @@
+"""
+prodsum: functions return difference between odds product and evens sum from list
+"""
 from typing import List
 
 
-def prodsum(l: List[int]) -> int:
+def prodsum(numbers: List[int]) -> int:
+    """
 
-    return 42
+    :param numbers: [int] list of odds and evens
+    :return: int difference between odds product and evens sum
+    """
+    odd = 0
+    even = 0
+
+    if len(numbers) >= 1:
+        odd = 1
+
+    for number in numbers:
+        if number % 2 == 0:
+            even += number
+        else:
+            odd *= number
+
+    return odd - even
