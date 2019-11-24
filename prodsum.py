dfb24a54d113f6f8efde77ef91577416da00655d
Andrii <andrewsotnikov88@gmail.com>
2019-11-24 21:45:00 +0200

    Prodsum solution

--- prodsum.py
+++ prodsum.py
@@ -1,6 +1,12 @@
+"""Find the difference between the product of the even and the sum of the odd list items"""
+import math
 from typing import List
 
 
-def prodsum(l: List[int]) -> int:
-
-    return 42
+def prodsum(list_of_elements: List[int]) -> int:
+    """Difference between the product of the even and the sum of the odd"""
+    if list_of_elements:
+        evens = list(filter(lambda x: (x % 2 == 0), list_of_elements))
+        odds = list(filter(lambda x: (x % 2 != 0), list_of_elements))
+        return math.prod(odds) - sum(evens)
+    return 0
