75efcdd6d641b2b4ce6f550f657eb09e2a304402
Platoniuks <platon@alterpost.org>
2019-11-24 13:04:24 +0200

    resolved

--- prodsum.py
+++ prodsum.py
@@ -1,6 +1,22 @@
+"""Find difference between
+product of items with even index
+and sum of items with odd index
+"""
+
 from typing import List
 
 
-def prodsum(l: List[int]) -> int:
+def prodsum(nums: List[int]) -> int:
+    """General function"""
+    if not nums:
+        return 0
 
-    return 42
+    even = 1
+    odd = 0
+
+    for index, value in enumerate(nums):
+        if index % 2 == 0:
+            even *= value
+        else:
+            odd += value
+    return even - odd
